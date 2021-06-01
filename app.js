a1e5bd4c1760d6464228279bc6140ee1bae186b4
Matheus Pimentel <matheus-ipero@hotmail.com>
2021-06-01 00:31:26 -0300

    select data ok

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 const express = require('express')
+const handleBars = require('express-handlebars')
 const { urlencoded } = require('body-parser')
+const mysql = require('mysql')
 
 const port = 3000
 const app = express()
 
+//Start
 app.listen(port, function(req,res){
     console.log('Server is running!')
 })
 
-//Routes
-app.get('/', function(req,res){
-    res.sendFile(__dirname+'/views/index.html')
+//Conection
+const sql = mysql.createPool({
+    host:'localhost',
+    database: 'test',
+    user:'root',
+    password:'',
+    port:3306
 })
 
-app.post('/insert', urlencoded, function(req,res){
+//CSS and js
+app.use('/lib/css', express.static('css'))
+app.use('/lib/js', express.static('js'))
 
+
+//Template
+app.engine('handlebars', handleBars({
+    defaultLayout:'main'
+}))
+app.set('view engine', 'handlebars')
+
+//Routes
+app.get('/', function(req,res){
+    sql.query('SELECT * FROM clientes', function(err, results, fields){
+        if(err) console.log(err)
+        else res.render('select', {data: results})
+    })
+})
+
+app.get('/update/:id', function(req,res){
+    res.render('update', {id: req.params.id})
 })
